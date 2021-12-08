1b70ba9b7d5468c14f5e58f6b5d15675e23c28bb
Ian Snyder <isnide23@gmail.com>
2021-12-08 12:24:07 -0800

    renmae to be more explicit References #1

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
 require_relative 'game'
-  require_relative 'box'
-  require_relative 'die'
+require_relative 'box'
+require_relative 'die'
 
 NUMBER_OF_TILES = 9
 NUMBER_OF_DICE = 2
 NUMBER_OF_SIDES = 6
 
-dice= (1..NUMBER_OF_DICE.map { |i| Die.new(NUMBER_OF_SIDES) }
+dice= (1..NUMBER_OF_DICE).map { |i| Die.new(NUMBER_OF_SIDES) }
 box= Box.new(NUMBER_OF_TILES)
 
 game=Game.new(box, dice)
@@ -15,23 +15,4 @@ puts Game::WELCOME_MESSAGE
  game.start!
 game.play until game.over?
 
-puts   game.results
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+puts game.results
